f4920537145f0338cb79d7e53b55ea82320ca385
Quentin RUHF <quentin.ruhf@gmail.com>
2024-12-05 20:47:54 +0100

    Add Colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'white': '#ffffff',
+        'perso-blue-100': '#9BCEE1',
+        'perso-blue-300': '#51B2D7',
+        'perso-blue-500': '#3F8CAF',
+        'perso-blue-700': '#285D81',
+        'perso-blue-900': '#1C5077',
+      },
+    },
   },
   plugins: [],
 }
